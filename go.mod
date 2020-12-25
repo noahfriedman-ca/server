0711ecadef53db01fa56fd0d8efc08d4bc11f15e
Noah Friedman <noahfriedman2@gmail.com>
2020-12-25 16:48:08 -0500

    Add router test [skip ci/cd]

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/noahfriedman-ca/server
 go 1.15
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/onsi/ginkgo v1.14.2 // indirect
-	github.com/onsi/gomega v1.10.4 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/onsi/ginkgo v1.14.2
+	github.com/onsi/gomega v1.10.4
 	unit.nginx.org v0.0.0-20201222145341-196b4d90c8cc
 )
--- router_test.go
+++ router_test.go
@@ -1,8 +1,92 @@
-package main_test
+package main
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"strings"
 )
 
-var _ = Describe("the router", func() {
-})
+var _ = Describe("the router",
+	func() {
+		DescribeTable("the different routes", func(uri string, localPath string) {
+			if _, e := os.Stat(localPath); os.IsNotExist(e) {
+				By("creating temporary file '" + localPath + "'")
+				if e := os.MkdirAll(path.Dir(localPath), os.ModePerm); e != nil {
+					GinkgoT().Error(e)
+				} else {
+					if f, e := os.Create(localPath); e != nil {
+						GinkgoT().Error(e)
+					} else {
+						if _, e := f.Write([]byte("<html></html>")); e != nil {
+							GinkgoT().Error()
+						}
+						f.Close()
+					}
+				}
+
+				defer func() {
+					if e := os.Remove(localPath); e != nil {
+						GinkgoT().Error(e)
+					}
+					for p := "./" + strings.TrimRight(path.Dir(localPath), "/"); p != "./"; p = "./" + strings.TrimRight(path.Dir(p), "/") {
+						if d, e := os.Open(p + "/"); e != nil {
+							GinkgoT().Error(e)
+						} else {
+							if n, e := d.Readdirnames(0); e != nil {
+								GinkgoT().Error(e)
+							} else {
+								if len(n) != 0 {
+									break
+								} else {
+									if e := os.Remove(p); e != nil {
+										GinkgoT().Error(e)
+									}
+								}
+							}
+						}
+					}
+				}()
+			}
+
+			By("opening '" + localPath + "'")
+			f, e := os.Open(localPath)
+			if e != nil {
+				GinkgoT().Error(e)
+			}
+			var local string
+			if b, e := ioutil.ReadAll(f); e != nil {
+				GinkgoT().Error(e)
+			} else {
+				local = string(b)
+			}
+			f.Close()
+
+			By("creating a mock server")
+			ms := httptest.NewServer(Router())
+			defer ms.Close()
+
+			By("requesting the URI '" + uri + "' from the mock server")
+			var result string
+			if r, e := http.Get(ms.URL + uri); e != nil {
+				GinkgoT().Error(e)
+			} else {
+				if b, e := ioutil.ReadAll(r.Body); e != nil {
+					GinkgoT().Error(e)
+				} else {
+					result = string(b)
+				}
+			}
+
+			Expect(result).To(Equal(local))
+		},
+			Entry("the '/' route", "/", "./static/build/index.html"),
+			Entry("the '/sitemap.xml' route", "/sitemap.xml", "./sitemap.xml"),
+			Entry("the '/projects/test/' route", "/projects/test/", "./projects/test/build/index.html"),
+		)
+	})
