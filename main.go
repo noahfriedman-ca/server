daef9641a0baf1406f021212eaae15c0757d64e0
Noah Friedman <noahfriedman2@gmail.com>
2020-12-25 15:37:57 -0500

    Add router.go and associated files [skip ci/cd]

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 import "unit.nginx.org/go"
 
 func main() {
-	if e := unit.ListenAndServe(":8080", nil); e != nil {
+	if e := unit.ListenAndServe(":8080", Router()); e != nil {
 		panic(e)
 	}
 }
---
+++ router.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/gorilla/mux"
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+
+	return r
+}
