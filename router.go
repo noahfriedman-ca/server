3e546c064ad3d7d67c867373f3ba82e6022817e7
Noah Friedman <noahfriedman2@gmail.com>
2020-12-25 17:13:26 -0500

    Implement router

--- router.go
+++ router.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
+
+	r.PathPrefix("/projects/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix(r.URL.Path, http.FileServer(http.Dir("."+r.URL.Path+"build/"))).ServeHTTP(w, r)
+	})
+	r.Path("/sitemap.xml").HandlerFunc(func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./sitemap.xml") })
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./static/build/"))))
 
 	return r
 }
