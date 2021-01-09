f9e8aa09a33f135ac67d580ccdac6e1eb3e82fc9
Noah Friedman <noahfriedman2@gmail.com>
2021-01-09 12:41:21 -0500

    FULLY remove api package

--- router.go
+++ router.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/noahfriedman-ca/server/api"
 	"net/http"
 )
 
@@ -12,7 +11,6 @@ func serveFile(w http.ResponseWriter, r *http.Request) {
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
-	api.Subrouter(r)
 
 	r.PathPrefix("/projects/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.StripPrefix(r.URL.Path, http.FileServer(http.Dir("."+r.URL.Path+"build/"))).ServeHTTP(w, r)
